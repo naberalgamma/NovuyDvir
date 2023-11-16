ac75d6c143b68ee91304401bf1a7673cf2b99438
Yurii Stupnytskyi <stupnytskyi.yurii.analityk1@gmail.com>
2023-11-16 11:53:08 +0200

    add js

--- Js/main.js
+++ Js/main.js
@@ -377,28 +377,28 @@ function showModalOrder() {
   orderGoodsWrapper.classList.add("active");
 }
 
-// const form = document.querySelector("#form");
-// const nameInput = document.querySelector("[name=name]");
-// const phone = document.querySelector("[name=phone]");
+const form = document.querySelector("#form");
+const nameInput = document.querySelector("[name=name]");
+const phone = document.querySelector("[name=phone]");
 
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   console.log(nameInput.value);
-//   console.log(phone.value);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(nameInput.value);
+  console.log(phone.value);
 
-//   fetch("http://127.0.0.1:5501/form", {
-//     method: "POST", // *GET, POST, PUT, DELETE, etc.
-//     mode: "cors", // no-cors, *cors, same-origin
-//     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-//     credentials: "same-origin", // include, *same-origin, omit
-//     headers: {
-//       "Content-Type": "application/json",
-//       // 'Content-Type': 'application/x-www-form-urlencoded',
-//     },
-//     redirect: "follow", // manual, *follow, error
-//     referrerPolicy: "no-referrer", // no-referrer, *client
-//     body: JSON.stringify({ name: nameInput.value, phone: phone.value }), // body data type must match "Content-Type" header
-//   }).then((response) => {
-//     console.log(response.status);
-//   });
-// });
+  fetch("http://127.0.0.1:5501/form", {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *client
+    body: JSON.stringify({ name: nameInput.value, phone: phone.value }), // body data type must match "Content-Type" header
+  }).then((response) => {
+    console.log(response.status);
+  });
+});
